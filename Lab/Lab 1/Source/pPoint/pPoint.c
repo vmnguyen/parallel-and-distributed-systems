d42a7c67f2495fe414c90e44a46dc625f4e49a5b
vmnguyen <51302307@hcmut.edu.vn>
2016-09-22 23:01:46 +0700

    Add Lab 2 and Lab 3

---
+++ Lab/Lab 1/Source/pPoint/pPoint.c
@@ -0,0 +1,31 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv){
+	// Define some variables to use in this program
+	int rank, size;
+	double a;
+	double b;
+	double s;
+	// Check status of MPI
+	MPI_Status status;
+	// Inititial MPI
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	fprintf(stdout, "\n Process %d of %d processes starts \n", rank, size);
+	if (rank == 1) {
+		b = 12.2;
+		MPI_Recv(&a,1,MPI_DOUBLE, MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,
+		&status);
+		s = b - a;
+		fprintf(stdout, " The result is : %f \n", s);
+	}
+	else {
+		a = rank;
+		MPI_Send(&a,1, MPI_DOUBLE, 1, 11, MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
+	return 0;
+}
